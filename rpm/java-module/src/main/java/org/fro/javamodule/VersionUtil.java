00cf467965a96d1896aeb4871e5e1eace3d34d9d
F. Rolland <frederic.rolland@prologism.fr>
2015-12-09 17:08:34 +0100

    Initial Commit

---
+++ rpm/java-module/src/main/java/org/fro/javamodule/VersionUtil.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.fro.javamodule;
+
+/**
+ * @author Cycomiko
+ *
+ */
+public class VersionUtil {
+
+	private static final String FUNCTIONAL_VERSION = "0.1.0";
+	
+	/**
+	 * Constructor
+	 */
+	public VersionUtil() {
+		super();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(FUNCTIONAL_VERSION);
+	}
+
+}
